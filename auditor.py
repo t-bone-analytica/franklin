27ab0a573b639bbc9d6207a683e1955af28b7bd2
Geoff Young <geoffreysyoung@gmail.com>
2018-08-08 20:09:45 -0400

    Add storing of property class

--- auditor.py
+++ auditor.py
@@ -1,11 +1,59 @@
 #!/usr/env python
 
+import os
+import time
 import requests
+import mysql.connector
 from bs4  import BeautifulSoup
 from lxml import html
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 SEARCH_FORM_URL = "http://property.franklincountyauditor.com/_web/search/commonsearch.aspx?mode=parid"
+
+
+def main():
+    status = 'OK'
+    while status == 'OK':
+        status = select_property_id()
+        print(status)
+        time.sleep(2)
+
+
+def select_property_id():
+    # Initialize database connection
+    db = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        passwd=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE")
+    )
+    cursor = db.cursor()
+
+    # Get the next property id to be classified
+    cursor.execute("SELECT `parcel_id` FROM `delinquency` WHERE `property_class` is NULL ORDER BY `id` ASC LIMIT 1")
+    result = cursor.fetchone()
+    try:
+        PROPERTY_ID = result[0]
+    except:
+        return 'ERROR'
+
+
+    PARCEL_CLASS_STRING = get_property_class(PROPERTY_ID)
+
+    classes = {
+        'R - Residential' : 1,
+        'C - Commercial'  : 2,
+        'E - Exempt'     : 3
+    }
+    PARCEL_CLASS = classes[PARCEL_CLASS_STRING]
+
+    store_property_class( PROPERTY_ID, PARCEL_CLASS )
+    time.sleep(3)
+
+
 
 def get_property_class(PARCEL_ID):
     # Create a persistent session
@@ -34,3 +82,25 @@ def get_property_class(PARCEL_ID):
 
     return tax_status_cells[1].contents[0]
 
+
+def store_property_class(PROPERTY_ID, PARCEL_CLASS):
+    # Initialize database connection
+    db = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        passwd=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE")
+    )
+    cursor = db.cursor()
+
+    sql = "UPDATE `delinquency` SET `property_class` = %s WHERE `parcel_id` = %s LIMIT 1"
+    val = ( str(PARCEL_CLASS), str(PROPERTY_ID) )
+
+    cursor.execute(sql, val)
+    db.commit()
+
+
+
+
+if __name__ == '__main__':
+    main()
